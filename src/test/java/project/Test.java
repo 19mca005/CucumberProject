3b5b4c1f89d5acab08c88c8deaf5e091e8acb88a
19mca005 <19mca005kumaresan.l@gmail.com>
2023-09-27 07:40:28 +0530

    project added

---
+++ src/test/java/project/Test.java
@@ -0,0 +1,19 @@
+package project;
+
+import SocialMediaIntelligence.target.classes.com.lk.self.intelligence.media.social.instagram.IGDetails;
+import SocialMediaIntelligence.target.classes.com.lk.self.intelligence.media.social.instagram.KumaresanL;
+
+public class Test  {
+
+	public static void main(String[] args) {
+    
+		KumaresanL ob = new IGDetails();
+		ob.followersCount("kula_kumaru");
+		
+		
+		
+		
+
+	}
+
+}
